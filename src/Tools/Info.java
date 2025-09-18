cd3be80e6c5d9809d32ae19e52f201fd49b7f4d2
JuanX-M <munozjuancruz6@gmail.com>
2025-09-18 14:40:20 -0300

    Agregue Primer Borrador de Tabla de Simbolos, HashMap<String,Info> String es el lexema e Info una clase

---
+++ src/Tools/Info.java
@@ -0,0 +1,26 @@
+package Tools;
+
+/**
+ * Representa una entrada en la Tabla de Símbolos.
+ * Almacena los atributos asociados de un lexema, como su tipo de token y nroLinea.
+ */
+class Info {
+
+    //TODO: Verificar atributos privados de Info
+    private String tokenType; // ej: "ID", "CTE_INT"
+
+    private int nroLinea;
+
+    public Info(String tokenType, int nroLinea) {
+        this.tokenType = tokenType;
+        this.nroLinea=nroLinea; // creo que se calcula por lexico, cambiar esto
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public int getNroLinea() {
+        return nroLinea;
+    }
+}
---
+++ src/Tools/TablaSimbolos.java
@@ -0,0 +1,37 @@
+package Tools;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Clase que implementa la Tabla de Símbolos
+public class TablaSimbolos {
+
+    //TODO: Consultar si les parece bien planteada la Tabla
+    private Map<String, Info> Tabla;
+
+    public TablaSimbolos() {
+        this.Tabla = new HashMap<>();
+    }
+
+    public void addSimboloTabla(String lexema, Info i) {
+        table.putIfAbsent(lexema, i);
+    }
+
+
+    public Info getSimbolo(String lexema) {
+        return Tabla.get(lexema);
+    }
+
+    public boolean containsSymbol(String lexema) {
+        return Tabla.containsKey(lexema);
+    }
+
+    public void printTabla() {
+        System.out.println("------ Contenido de la Tabla de Símbolos ------");
+        for (Map.Entry<String, Info> entry : Tabla.entrySet()) {
+            System.out.println("Lexema: '" + entry.getKey() + "' -> " + entry.getValue().toString());
+        }
+        System.out.println("---------------------------------------------");
+    }
+}
