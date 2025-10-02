813ec12ebf49039ab0681bf455e4acd597c8ae6d
JuanX-M <munozjuancruz6@gmail.com>
2025-10-02 17:04:14 -0300

    Se cambio case "/n" por condicion if en metodo convertir por error de cadena 1 linea

--- src/Lexico/AccionesSemanticas/ASBorrarComentariosML.java
+++ src/Lexico/AccionesSemanticas/ASBorrarComentariosML.java
@@ -11,6 +11,7 @@ public class ASBorrarComentariosML extends AccionSemantica{
 
     @Override
     public Pair<String, Integer> run(Character simbolo, Cursor cursor) {
+        System.out.println("buffer " + BUFFER.toString());
         BUFFER.setLength(0);
         return new Pair<>(null, null); //no es simplemente null porque si fuera null es porque hay un error
     }
