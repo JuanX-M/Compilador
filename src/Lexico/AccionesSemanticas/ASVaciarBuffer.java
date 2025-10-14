9c48d25de31f79a5d2161e9cfe6db091e8baceba
nicoortiz11 <nico.ortiz571@gmail.com>
2025-10-14 18:09:30 -0300

    Nueva AS

---
+++ src/Lexico/AccionesSemanticas/ASVaciarBuffer.java
@@ -0,0 +1,19 @@
+package Lexico.AccionesSemanticas;
+
+import Tools.Cursor;
+import Tools.Pair;
+
+public class ASVaciarBuffer extends AccionSemantica{
+
+    public ASVaciarBuffer() {
+        super(13);
+        //this.palabrasReservadas = transformarArraylist();
+    }
+
+    @Override
+    public Pair<String, Integer> run(Character simbolo, Cursor cursor) {
+        BUFFER.setLength(0);
+        return new Pair<>(null,0);
+    }
+
+}
